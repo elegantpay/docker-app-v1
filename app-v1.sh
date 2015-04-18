a50d48654e7e99cc89736acbd69f830529139097
yinheli <me@yinheli.com>
2015-04-18 23:12:47 +0800

    fix

--- app-v1.sh
+++ app-v1.sh
@@ -7,8 +7,8 @@ if [ ! -f "$init_lock_file" ]; then
     SSHD_PORT=${SSHD_PORT:-22}
     MYSQL_PORT=${MYSQL_PORT:-3306}
 
-    sed -ri "s/Port\s+22/Port $(SSHD_PORT)/g" /etc/ssh/sshd_config
-    sed -ri "s/MYSQL_PORT/$(MYSQL_PORT)/g" /etc/mysql/my.cnf
+    sed -ri "s/Port\s+22/Port $SSHD_PORT/g" /etc/ssh/sshd_config
+    sed -ri "s/MYSQL_PORT/$MYSQL_PORT/g" /etc/mysql/my.cnf
     
     touch $init_lock_file
 
